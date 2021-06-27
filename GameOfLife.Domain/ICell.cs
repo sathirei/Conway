abb197508cd860d0cbff76173362014162a2f10f
Yumnam Arunkumar Singh <sathirei@gmail.com>
2021-06-27 21:05:34 +0530

    Initial commit

---
+++ GameOfLife.Domain/ICell.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife.Domain
+{
+    interface ICell
+    {
+        public List<ICell> Neighbors { get; set; }
+    }
+}
